8be909e074b91f0c3a9bf54d80a9e10b34aa9a47
Gabriel <gabborges37@gmail.com>
2022-11-10 12:20:23 -0300

    Crossover_operation 2.0

--- 7_crossover_operation/crossover_operation.py
+++ 7_crossover_operation/crossover_operation.py
@@ -1,4 +1,4 @@
-import sys, random, string
+import sys, random, string, poplib
 from PyQt5 import QtCore, uic
 from PyQt5.QtWidgets import QApplication, QComboBox, \
     QPushButton, QLineEdit, QLabel
@@ -25,9 +25,9 @@ def on_cross_pushbutton_clicked():
 def on_method_combobox_current_text_changed():
     if method_combo_box.currentText()=="PMX":
         father_line_edit.setInputMask('AAAAAAAAAA')
-        father_line_edit.setText('AAAAAAAAAA')
+        father_line_edit.setText('ABCDEFGHIJ')
         mother_line_edit.setInputMask('AAAAAAAAAA')
-        mother_line_edit.setText('BBBBBBBBBB')  
+        mother_line_edit.setText('JHIGFEDCBA')  
     else:
         father_line_edit.setInputMask('9999999999')
         father_line_edit.setText('0000000000')
@@ -36,16 +36,49 @@ def on_method_combobox_current_text_changed():
     print('combo box changed')
 
 def pmx_crossover():
+    cromossomopai= father_line_edit.text()
+    cromossomomae= mother_line_edit.text()
+    corte1 = random.randint(0,len(cromossomopai)-2)
+    corte2 = random.randint(corte1+1,len(cromossomomae)-1)
+    cortepai=''
+    cortemae=''
+    corte1pai=''
+    corte1mae=''
+    corte2pai=''
+    corte2mae=''
+    for i in range(0,corte1):
+        cortepai=cortepai+(cromossomopai[i])
+        corte1mae= corte1mae+(cromossomomae[i])
+    for i in range(corte1,corte2):
+        cortemae=cortemae+(cromossomomae[i])
+        corte1pai= corte1pai+(cromossomopai[i])
+    for i in range(corte2,10):
+        corte2pai=corte2pai+(cromossomopai[i])
+        corte2mae= corte2mae+(cromossomomae[i])
+    # Troca de caracteres repetidos Filho 1
+    for i in range(0,len(cortemae)):
+        for j in range(0,len(cortepai)):
+            if (cortemae[i]==cortepai[j]):
+                cortepai=cortepai.replace(cortepai[j],corte1pai[i])
+            
+            if (corte1pai[i]==corte1mae[j]):
+                corte1mae=corte1mae.replace(corte1mae[j],cortemae[i])
+        for h in range(0,len(corte2pai)):
+            if (cortemae[i]==corte2pai[h]):
+                corte2pai=corte2pai.replace(corte2pai[h],corte1pai[i])
+            
+            if (corte1pai[i]==corte2mae[h]):
+                corte2mae=corte2mae.replace(corte2mae[h],cortemae[i])
     # Esta função está retornando 6 valores.
     # Ao criar o corpo da função você deve ordená-los de acordo com
     # as linhas 17 a 22 deste arquivo.
-    return '','','','','',''
+    return cortepai,cortemae,corte2pai,corte1mae,corte1pai,corte2mae
 
 
 def simple_cut_crossover():
     cromossomopai= father_line_edit.text()
     cromossomomae= mother_line_edit.text()
-    corte= random.randint(0,9)
+    corte= random.randint(0,len(cromossomopai)-1)
     cortepai=''
     cortemae=''
     corte1pai=''
